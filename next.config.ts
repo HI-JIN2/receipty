0a23a4ab50195318de04503a8ceb421eed74c385
Yu Jin <qldls0307@naver.com>
2026-01-23 23:25:16 +0900

    fix(image): allow TMDB remote images

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "shopping-phinf.pstatic.net",
       },
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
     ],
   },
 };
